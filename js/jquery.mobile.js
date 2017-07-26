5d46776d28278ef3d5e8fa66586804bc77dba3ef
Brad West <brad@bradonomics.com>
2017-07-26 11:41:37 -0500

    Test data-src -> src logic

--- js/jquery.mobile.js
+++ js/jquery.mobile.js
@@ -1,13 +1,11 @@
-if (jQuery.browser.mobile != true) {
-  console.log("is desktop"); // it is mobile browser
+if (jQuery.browser.mobile) {
+  $(".videoContainer").css( "display", "none" );
+} else {
+  var sources = document.querySelectorAll('.videoContainer video source');
+  var video = document.querySelector('.videoContainer video');
+  for (var i = 0; i < sources.length; i++) {
+    sources[i].setAttribute('src', sources[i].getAttribute('data-src'));
+  }
+  video.load();
+  video.muted = "muted";
 }
-// else {
-//   console.log("is desktop"); // no mobile browser
-  // var sources = document.querySelectorAll('video.demo-video source, video.demo-video img'); // Define the video object this source is contained inside
-  // var video = document.querySelector('video.demo-video');
-  // for (var i = 0; i < sources.length; i++) {
-  //   sources[i].setAttribute('src', sources[i].getAttribute('data-src'));
-  // }
-  // video.load();
-  // video.muted = "muted";
-// }
